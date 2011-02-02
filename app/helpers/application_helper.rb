7afbc01fcd5c7355a7abfaf609a8d033bac25d0d
JuanCarlos <jjuanchow@gmail.com>
2011-02-02 16:35:44 -0600

    Eliminar la paginaa index por default

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   #Regresa un titulo por cada página
   def title
     base_title = "App de ejemplo del Tutorial de Ruby on Rails"
-    if @title.nil ?
+    if @title.nil?
       base_title
     else
       "#{base_title} | #{@title}"
